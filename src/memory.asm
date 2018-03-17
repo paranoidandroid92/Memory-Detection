03e63e614db234096fd50103228fd6656b6619f5
Ali ATIL <alihaydar.atil@gmail.com>
2018-03-17 23:22:11 +0300

    source code

--- src/memory.asm
+++ src/memory.asm
@@ -16,13 +16,11 @@ mov sp,0
 
 
 	call detect_memory
-	mov bl,[entry_count]
 	call print_memory_map
-	mov ax,0xb800
-	mov es,ax
-	mov di,0x42
-	mov ax,0x4e2D
-	mov [es:di],ax
+	mov ax,bar_string;
+	mov si,ax;
+	call printStr;
+	mov bl,[entry_count]
 end:
 	hlt
 	jmp end
@@ -132,10 +130,51 @@ letter:
 	add al,55
 	ret
 
+;prints string in [ds:si] to screen	
+printStr:
+	pusha;
+	push di
+	push es
+	cld;inc si after lodsb
+loadchr:
+	lodsb;
+	cmp al,0x00;
+	je printStrFinish;
+	cmp al,0x0A;
+	je newline;
+	mov ah,0x4e;
+	mov di,[video_memory_index];
+	mov es,[video_memory_base];
+	mov [es:di],ax;
+	inc di;
+	inc di;
+	mov [video_memory_index],di;
+	jmp loadchr;
+newline:
+	push dx;
+	push bx;
+	push ax;
+	mov bx,0x00A0;
+	add [video_memory_index],bx;
+	mov dx,0;
+	mov ax,[video_memory_index];
+	div bx;
+	sub [video_memory_index],dx;
+	pop ax;
+	pop bx;
+	pop dx;
+	jmp loadchr;
+printStrFinish:
+	pop es
+	pop di
+	popa;
+	ret;
 
-
+video_memory_base dw 0xb800
+video_memory_index dw 0x0000
 entry_count db 0x00
-screen_index dw 96
+screen_index dw 256
+bar_string db '      Type            Length            Base      ', 0x00
 times 510-($-$$) db 0x00
 db 0x55
 db 0xAA
